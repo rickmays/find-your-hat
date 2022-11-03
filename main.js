2db0d794b2481edf4611140cf4153e1ed6cfa2b1
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-11-03 14:48:14 -0500

    add generateField method

--- main.js
+++ main.js
@@ -8,25 +8,41 @@ const pathCharacter = "*";
 class Field {
   constructor(field) {
     this.field = field;
-    this.height = 10;
-    this.width = 10;
+    this.height = field.length;
+    this.width = field[0].length;
     this.currentRow = 0;
     this.currentColumn = 0;
     this.foundHat = false;
   }
 
+  static generateField() {
+    const fieldHeight = prompt("Enter desired height of field: ");
+    const fieldWidth = prompt("Enter desired width of field: ");
+    const field = Array.from(Array(Number(fieldHeight)), () => {
+      return new Array(Number(fieldWidth)).fill(fieldCharacter);
+    });
+    return field;
+  }
+
   print() {
     let printField = "";
     this.field.forEach((row) => {
+      // console.log(`ROW: ${row}`);
       printField += row.join("");
       printField += "\n";
     });
     console.log(printField);
+    // console.log(this.height);
+    // console.log(this.width);
   }
 
   play() {
     let move = "";
     while (!this.foundHat) {
+      console.clear();
+      this.print();
+      console.log(`row: ${this.currentRow}`);
+      console.log(`column: ${this.currentColumn}`);
       move = prompt("Which way? (d)own, (u)p, (l)eft, (r)ight: ");
       switch (move) {
         case "d":
@@ -45,14 +61,6 @@ class Field {
           break;
       }
 
-      if (this.field[this.currentRow][this.currentColumn] === hat) {
-        console.log("Congratulations, you found your hat!");
-        break;
-      }
-      if (this.field[this.currentRow][this.currentColumn] === hole) {
-        console.log("Sorry, you fell into a hole! GAME OVER!!");
-        break;
-      }
       if (
         this.currentRow < 0 ||
         this.currentRow === this.height ||
@@ -62,24 +70,31 @@ class Field {
         console.log("Out of bounds, GAME OVER!!");
         break;
       }
-      console.log(`row: ${this.currentRow}`);
-      console.log(`column: ${this.currentColumn}`);
+
+      if (this.field[this.currentRow][this.currentColumn] === hat) {
+        console.log("Congratulations, you found your hat!");
+        break;
+      }
+      if (this.field[this.currentRow][this.currentColumn] === hole) {
+        console.log("Sorry, you fell into a hole! GAME OVER!!");
+        break;
+      }
+      this.field[this.currentRow][this.currentColumn] = pathCharacter;
     }
   }
 }
 
-const myField = new Field([
-  ["*", "O", "░", "░", "O", "░", "░", "░", "░", "░"],
-  ["░", "░", "░", "░", "░", "░", "░", "O", "░", "░"],
-  ["░", "░", "░", "░", "░", "░", "░", "░", "░", "░"],
-  ["░", "░", "░", "░", "░", "░", "░", "O", "░", "░"],
-  ["░", "░", "░", "░", "O", "░", "░", "░", "░", "░"],
-  ["░", "░", "░", "░", "░", "O", "░", "░", "░", "░"],
-  ["░", "░", "░", "O", "░", "░", "░", "░", "░", "░"],
-  ["░", "░", "░", "░", "░", "O", "░", "░", "░", "░"],
-  ["░", "O", "O", "░", "░", "░", "░", "░", "░", "░"],
-  ["░", "O", "^", "░", "░", "░", "░", "░", "░", "░"],
-]);
-
-myField.print();
+// const myField = new Field([
+//   ["*", "O", "░", "░", "O", "░", "░", "░", "░", "░"],
+//   ["░", "░", "░", "░", "░", "░", "░", "O", "░", "░"],
+//   ["░", "░", "░", "░", "░", "░", "░", "░", "░", "░"],
+//   ["░", "░", "░", "░", "░", "░", "░", "O", "░", "░"],
+//   ["░", "░", "░", "░", "O", "░", "░", "░", "░", "░"],
+//   ["░", "░", "░", "░", "░", "O", "░", "░", "░", "░"],
+//   ["░", "░", "░", "O", "░", "░", "░", "░", "░", "░"],
+//   ["░", "░", "░", "░", "░", "O", "░", "░", "░", "░"],
+//   ["░", "O", "O", "░", "░", "░", "░", "░", "░", "░"],
+//   ["░", "O", "^", "░", "░", "░", "░", "░", "░", "░"],
+// ]);
+const myField = new Field(Field.generateField());
 myField.play();
